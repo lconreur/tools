4afba982240b5be4fa362c9513ce49d6353bcdf3
lconreur <laurence.conreur@etu.univ-amu.fr>
2025-07-28 10:07:00 +0200

    delete old file renomme

--- renomme.py
+++
@@ -1,8 +0,0 @@
-mkdir -p renomme
-find . -type f -name 'scaffold_*' | while read -r file; do
-    dir=$(basename "$(dirname "$file")")
-    prefix=${dir:0:6}
-    base=$(basename "$file")
-    newname="${prefix}_${base}"
-    cp "$file" "renomme/$newname"
-done
