983c2eb3e0a28c9b7a191c9855159d5628e3e506
lconreur <laurence.conreur@etu.univ-amu.fr>
2025-07-30 10:57:13 +0200

    corr

--- Selecting_a_complete_set_of_distance_below_cutoff.sql
+++ Selecting_a_complete_set_of_distance_below_cutoff.sql
@@ -11,13 +11,13 @@ SELECT substr(gbka.path, length(gbka.path) - instr(reverse(gbka.path), '/') + 2)
   d.edge_param_id,
   params.weights
 FROM distance d
-INNER JOIN bgc_record bgca ON bgca.id=d.record_a_id
-INNER JOIN bgc_record bgcb ON bgcb.id=d.record_b_id
-INNER JOIN gbk gbka ON gbka.id=bgca.gbk_id
-INNER JOIN gbk gbkb ON gbkb.id=bgcb.gbk_id
-INNER JOIN edge_params params ON d.edge_param_id==params.id
+INNER JOIN bgc_record bgca ON bgca.id = d.record_a_id
+INNER JOIN bgc_record bgcb ON bgcb.id = d.record_b_id
+INNER JOIN gbk gbka ON gbka.id = bgca.gbk_id
+INNER JOIN gbk gbkb ON gbkb.id = bgcb.gbk_id
+INNER JOIN edge_params params ON d.edge_param_id = params.id
 INNER JOIN Organisms o ON gbka.organism = o.alias
 INNER JOIN Taxonomy t ON o.taxo_id = t.id 
-WHERE d.distance<0.3 and d.distance != 0
+WHERE d.distance < 0.3 and d.distance != 0
 ORDER BY d.distance
-LIMIT 3000;
+--LIMIT 3000;
