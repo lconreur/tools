70e621de60b8d9971aedc0fa238d47edfb5abd4c
lconreur <laurence.conreur@etu.univ-amu.fr>
2025-08-04 12:36:08 +0200

    counts des tables

---
+++ counts des tables.sql
@@ -0,0 +1,35 @@
+select count(*) -- 619100 lignes
+from cds
+
+select count(*) -- 619100 lignes
+from distance d 
+
+select count(*) -- 8.094 lignes
+from bgc_record br 
+
+select count(*) -- 1.820 lignes
+from gbk g 
+
+select count(*) -- 14 lignes
+from Taxonomy t 
+
+select count(*) -- 66 lignes
+from Organisms o 
+
+select count(*) -- 7 lignes
+from edge_params ep 
+
+select count(*) -- 862 lignes
+from connected_component cc 
+
+select count(*) -- 862 lignes
+from bgc_record_family brf 
+
+select count(*) -- 182 lignes
+from family f 
+
+select count(*) -- 1 ligne
+from run r 
+
+select count(*) -- 27.485 lignes
+from scanned_cds sc 
