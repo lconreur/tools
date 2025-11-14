071d03a70c3085b9f85c773240a35562a09be41e
lconreur <laurence.conreur@etu.univ-amu.fr>
2025-11-14 09:22:34 +0100

    query to retrieve all 970 BGC lines with details

---
+++ detail_organism_scaffold_cat-prod-fam.sql
@@ -0,0 +1,10 @@
+select distinct g.id, g.organism , g.description , o.Taxo_Clade , o.Genre_espece , br.id AS 'bgc_record.id', br.category , br.product, cc.bin_label , c.gene_kind , h.id as 'hsp.id', h.cds_id , h.accession , h.env_start , h.env_stop , h.bit_score, c.orf_num , brf.family_id 
+from gbk g 
+inner join Organisms o on g.organism = o.alias 
+inner join cds c on g.id = c.gbk_id 
+inner join bgc_record br on g.id = br.gbk_id 
+inner join connected_component cc on br.id = cc.record_id 
+inner join hsp h on c.id = h.cds_id 
+inner join bgc_record_family brf on br.id = brf.record_id 
+group by g.id, g.organism , g.description , o.Taxo_Clade , o.Genre_espece , br.id, br.category , br.product
+order by g.organism , g.description , o.Taxo_Clade , o.Genre_espece , br.id, br.category , br.product
